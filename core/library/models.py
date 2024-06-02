ba71fc08ba0fa047c94dce43bfff151385bdd136
WiseVizo <mecoc1011@gmail.com>
2024-06-02 21:42:05 +0530

    connecting backend data with frontend

--- core/library/models.py
+++ core/library/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
-# class Book(models.Model):
-#     title = models.CharField(max_length=255)
-#     author = models.CharField(max_length=255)
-#     description = models.TextField(blank=True, null=True)
-#     image = models.ImageField(upload_to='book_images/', blank=True, null=True)
-#     quantity = models.PositiveIntegerField()
+class Book(models.Model):
 
-#     def __str__(self):
-#         return self.title
+    SEMESTER_CHOICES = [
+        (1, 'Semester 1'),
+        (2, 'Semester 2'),
+        (3, 'Semester 3'),
+        (4, 'Semester 4'),
+        (5, 'Semester 5'),
+        (6, 'Semester 6'),
+    ]
+
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='img/', default='img/placeholder_book.webp')
+    quantity = models.PositiveIntegerField()
+    semester = models.PositiveSmallIntegerField(choices=SEMESTER_CHOICES)
+
+    def __str__(self):
+        return self.name
