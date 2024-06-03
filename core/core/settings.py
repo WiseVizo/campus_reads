d09dbf548ea69b1c712c2e09d7006abff5a117e0
WiseVizo <mecoc1011@gmail.com>
2024-06-03 11:00:43 +0530

    user auth implemented

--- core/core/settings.py
+++ core/core/settings.py
@@ -125,3 +125,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'students.CustomUser'
+
+LOGIN_REDIRECT_URL = 'library:home_page'
+LOGIN_URL = 'students:login'
