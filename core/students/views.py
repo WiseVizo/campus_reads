c8ca3c8f0d18a21e587069535daec77bab9dfbf8
WiseVizo <mecoc1011@gmail.com>
2024-06-03 09:49:25 +0530

    user creation and registration done

--- core/students/views.py
+++ core/students/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page or login page
+            return redirect('library:home_page')  # Adjust this to your login URL
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'students/register.html', {'form': form})
